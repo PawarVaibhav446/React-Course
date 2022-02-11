fa50402d881940f97dff8cb0036fd7250aa27045
Vaibhav Pawar <pawarvaibhav446@gmail.com>
2022-02-11 19:57:19 +0530

    First Day(1)

---
+++ Day1/index.js
@@ -0,0 +1,34 @@
+/*
+
+Imperative Way: (Vanilla JS)
+let h1 = document.createElement("h1")
+h1.textContent = "My Name is by far the best among you all present here"
+h1.className = "header"
+document.getElementById("root").append(h1)
+
+Declarative Way: (React JS)
+ReactDOM.render(<h1>Hello!!</h1>, document.getElementById("root"))
+
+Custom Component: 
+function MyName() {
+    return (
+        <ul>
+            <li>Vaibhav</li>
+            <li>Tukaram</li>
+            <li>Pawar</li>
+        </ul>
+    )
+}
+
+ReactDOM.render(<div>
+    <MyName />
+</div>, document.getElementById("root")
+)
+
+
+
+*/
+
+
+
+
