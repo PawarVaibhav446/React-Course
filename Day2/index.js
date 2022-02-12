3189d6536c0d5f2f3192a7d91502f8cf474dfcbf
Vaibhav Pawar <pawarvaibhav446@gmail.com>
2022-02-12 11:37:35 +0530

    2nd day of learning react

---
+++ Day2/index.js
@@ -0,0 +1,67 @@
+
+// let main = <div>
+//     <h1>My 2nd Day Of learning React.</h1>
+//     <p>React is too much fun and i love it!!!!</p>
+// </div>
+// console.log(main);
+
+// ReactDOM.render(
+//     <div>
+//         <h1>My 2nd Day Of learning React.</h1>
+//         <p>React is too much fun and i love it!!!!</p>
+//     </div>,
+//     document.getElementById("root")
+// )
+// //or
+// ReactDOM.render(
+//     main,
+//     document.getElementById("root")
+// )
+
+
+// Challenge 1
+// const pr = (
+//     <nav>
+//         <h1>Royal Homoeo Pharmacy</h1>
+//         <ul>
+//             <li>Pricing</li>
+//             <li>About</li>
+//             <li>Contact</li>
+//         </ul>
+//     </nav>
+// ) 
+
+// ReactDOM.render(pr, document.getElementById("root"))
+
+// Challenge 2
+// const jsx = (
+//     <nav>
+//         <h2>Hello 2nd Challenge of the day.</h2>
+//         <li>Pricing</li>
+//         <li>About</li>
+//         <li>Contact</li>
+//         <p>Hello How are you ? will i able to complete this course...............</p>
+//     </nav>
+// )
+
+// document.getElementById("root").append(jsx)
+// document.getElementById("root").append(JSON.stringify(jsx))
+
+// Challenge 3
+// const sample = (
+//     <div>
+//         <div>
+//             <img src="/Day2/ReactLogo.png" alt="#" width="40px"></img>
+//             <h1>Fun facts about React</h1>
+//             <ul>
+//                 <li>Was first released in 2013</li>
+//                 <li>Was originally created by Jordan Walke</li>
+//                 <li>Has well over 100K starts on Github</li>
+//                 <li>Is maintained by Facebook</li>
+//                 <li>Powers thousands of mobile apps, including enterprise apps</li>
+//             </ul>
+//         </div>
+//     </div>
+// )
+
+// ReactDOM.render(sample, document.getElementById("root"))
